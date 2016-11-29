d7f2e3a089863887823edeb11775b3b8dc54348b
Hong SH <hoihoi207@naver.com>
2016-11-29 11:16:50 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
    public static void main(String[] args){
       System.out.println("Hello Git");
-      System.out.println("Hellow My Git");
+      System.out.println("Hello My Git");
+      System.out.println("Hello His Git");
    }
 
 }
